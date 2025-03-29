5111e2c957851fceb8024d4487a70c5987d08824
athulraj9x <athulraj9xtechnologies@gmail.com>
2025-03-29 15:02:15 +0000

    pending rpc

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,12 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { RabbitMQModule } from './rabbitMQ/rabbitmq.module';
-import { GRPCModule } from './GRPC.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     RabbitMQModule,
     DatabaseModule,
-    GRPCModule,
     AuthModule,
   ],
   controllers: [],
